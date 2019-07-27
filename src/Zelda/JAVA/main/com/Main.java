20223350f0e85b2dbd5a4639a1a7720f90d54ee9
brunosmarques <brunosmarques1995@hotmail.com>
2019-07-27 16:01:44 -0300

    Mudanças nomes de projeto

---
+++ src/Zelda/JAVA/main/com/Main.java
@@ -0,0 +1,10 @@
+package Zelda.JAVA.main.com;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+       Graficos graficos = new Graficos(60.0);
+       graficos.Iniciar();
+    }
+}
